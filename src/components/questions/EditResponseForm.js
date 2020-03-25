7216c230741bb0418ff3336c27b36ed22680c36c
Ryan Crowley <ryancrowleycode@gmail.com>
2020-03-25 12:11:15 -0500

    EditResponseForm button text changes to Add Response or Update Response depending on whether there is a response already

--- src/components/questions/EditResponseForm.js
+++ src/components/questions/EditResponseForm.js
@@ -23,6 +23,7 @@ class EditResponseForm extends Component {
         question: '',
         isFromInterviewer: false,
         response: '',
+        buttonLabel: "Add Response",
         loadingStatus: false,
         open: false
     }
@@ -36,17 +37,21 @@ class EditResponseForm extends Component {
     getThenUpdateState = () => {
         apiManager.get(`questions/${this.questionId}`)
             .then(question => {
-                question.answer !== undefined && question.answer !== null ?
+                question.answer !== undefined 
+                && question.answer !== null
+                && question.answer !== "" ?
                     this.setState({
                         question: question.question,
                         isFromInterviewer: question.is_from_interviewer,
                         response: question.answer,
+                        buttonLabel: "Update Response",
                         loadingStatus: false,
                         open: false
                     })
                     : this.setState({
                         question: question.question,
                         isFromInterviewer: question.is_from_interviewer,
+                        buttonLabel: "Add Response",
                         loadingStatus: false,
                         open: false
                     })
@@ -80,15 +85,14 @@ class EditResponseForm extends Component {
     }
 
     render() {
-        return (
+\        return (
             <React.Fragment>
                 <button
                     type="button"
                     className="edit-response-button btn btn-success btn-sm"
                     onClick={() => this.setState({ open: true })}
                     disabled={this.state.loadingStatus}
-                >
-                    Update Response
+                >{this.state.buttonLabel}
                 </button>
                 <Modal
                     show={this.state.open}
